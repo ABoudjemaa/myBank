efbd0a7be411ec39431268ab64685dc57a9ca867
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-18 17:03:12 +0200

    -

---
+++ front/src/app/operations/layout.tsx
@@ -0,0 +1,13 @@
+const Layout = ({
+                    children,
+                }: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <div className={"max-w-6xl mx-auto"}>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
