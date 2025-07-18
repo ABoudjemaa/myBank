bc34c95bdecb45ddb4bb8ae456326bc0c85a5e1a
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-18 14:51:13 +0200

    -

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -3,7 +3,11 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\Symfony\Action\NotFoundAction;
+use App\Controller\MeController;
 use App\Dto\UserRegisterInput;
 use App\Repository\UserRepository;
 use App\State\UserRegistrationProcessor;
@@ -11,10 +15,39 @@ use Doctrine\ORM\Mapping as ORM;
 use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ApiResource(
     operations: [
+        new GetCollection(
+            uriTemplate: 'me',
+            controller: MeController::class,
+            paginationEnabled: false,
+            normalizationContext: [
+                'groups' => [
+                    'user:id',
+                    'user.firstName',
+                    'user.lastName',
+                    'user.email',
+                    'user.roles',
+                ],
+                'openapi_definition_name' => 'User.read',
+            ],
+            security: "is_granted('ROLE_USER')",
+            read: false,
+        ),
+        new Get(
+            controller: NotFoundAction::class,
+            openapi: false,
+            normalizationContext: [
+                'openapi_context' => [
+                    'summary' => 'hidden',
+                ],
+            ],
+            output: false,
+            read: false,
+        ),
         new Post(
             uriTemplate: 'register',
             normalizationContext: ['groups' => [
@@ -33,26 +66,37 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, JWTUser
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user:id'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user.firstName'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user.lastName'])]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user.email'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
 
     #[ORM\Column]
+    #[Groups(['user.roles'])]
     private array $roles = [];
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
     }
 
     public function getFirstName(): ?string
@@ -121,6 +165,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, JWTUser
     public static function createFromPayload($username, array $payload): static
     {
         $user = new self();
+//        $user->setId($payload['id']);
         $user->setEmail($username);
         $user->setRoles($payload['roles'] ?? []);
 
