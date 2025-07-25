ea28275a16000e5b9e8426ffcbf3d65f51acd0d4
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-25 20:38:18 +0200

    -

--- jenkins/frontend_build_image.groovy
+++ jenkins/frontend_build_image.groovy
@@ -14,6 +14,7 @@ stage('Deploy Frontend') {
     ]) {
         sh """
             ssh -i \$SSH_KEY ${REMOTE_USER}@${REMOTE_HOST} '
+                docker pull ${DOCKERHUB_USERNAME}/mybank_front:latest &&
                 docker stop mybank_front || true &&
                 docker rm mybank_front || true &&
                 docker run -d --name mybank_front -p 3001:3000 ${DOCKERHUB_USERNAME}/mybank_front:latest
