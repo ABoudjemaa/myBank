5d4257e133fc3d249e9fbf7e9373bcc8745f54bd
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-27 11:06:21 +0200

    -

--- jenkins/frontend_install.groovy
+++ jenkins/frontend_install.groovy
@@ -7,6 +7,7 @@ stage('Install & Run Frontend') {
         sh 'npm install'
         sh 'ls -la'
         sh "echo 'NEXT_PUBLIC_API_BASE_URL=http://backend/api' > .env"
+        sh 'cat .env'
         sh 'npx vitest run'
     }
     
