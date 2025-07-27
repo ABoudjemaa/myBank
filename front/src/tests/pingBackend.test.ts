af5a1c77f0292a287eba2c9da5d80d554b4d8722
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-27 11:31:05 +0200

    - add tests

--- front/src/tests/pingBackend.test.ts
+++ front/src/tests/pingBackend.test.ts
@@ -1,5 +1,6 @@
 import { pingBackend } from "./pingBackend";
 import { expect, test } from "vitest";
+import 'dotenv/config';
 
 test("API backend doit répondre avec succès", async () => {
     const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
