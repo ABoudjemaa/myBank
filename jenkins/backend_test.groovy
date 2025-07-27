c47c673596cccced76da3ab90d913ceaa6c5b7a4
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-27 12:58:37 +0200

    final

--- jenkins/backend_test.groovy
+++ jenkins/backend_test.groovy
@@ -2,40 +2,32 @@ node("${AGENT_DOCKER}") {
     stage('Prepare Environment and Docker Compose') {
         git branch: 'main', url: 'https://github.com/ABoudjemaa/myBank.git'
         dir('api') {
-//             sh '''
-//                 docker compose down -v --remove-orphans
-//                 rm -rf var/cache/*
-//                 rm -rf vendor composer.lock
-//                 rm -f .env .env.test .env.local .env.test.local
-//             '''
             // Create .env file before starting services
-            sh """
-                    echo \"APP_ENV=dev
-                APP_DEBUG=0
-                APP_SECRET=${APP_SECRET}
-                DATABASE_URL=mysql://root:root@database:3306/mybank-api-database?serverVersion=9.1.0&charset=utf8mb4
-                CORS_ALLOW_ORIGIN=${CORS_ALLOW_ORIGIN}
-                JWT_SECRET_KEY=${JWT_SECRET_KEY}
-                JWT_PUBLIC_KEY=${JWT_PUBLIC_KEY}
-                JWT_PASSPHRASE=${JWT_PASSPHRASE}
-                ADMIN_EMAIL=boudjemaa.amine.2003@gmail.com
-                ADMIN_PASSWORD=12345678\" > .env
-            """
+//             sh """
+//                     echo \"APP_ENV=dev
+//                 APP_DEBUG=0
+//                 APP_SECRET=${APP_SECRET}
+//                 DATABASE_URL=mysql://root:root@database:3306/mybank-api-database?serverVersion=9.1.0&charset=utf8mb4
+//                 CORS_ALLOW_ORIGIN=${CORS_ALLOW_ORIGIN}
+//                 JWT_SECRET_KEY=${JWT_SECRET_KEY}
+//                 JWT_PUBLIC_KEY=${JWT_PUBLIC_KEY}
+//                 JWT_PASSPHRASE=${JWT_PASSPHRASE}
+//                 ADMIN_EMAIL=boudjemaa.amine.2003@gmail.com
+//                 ADMIN_PASSWORD=12345678\" > .env
+//             """
 
-            sh """
-                echo \"APP_ENV=test
-                KERNEL_CLASS=App\\\\Kernel
-                APP_SECRET=${APP_SECRET}
-                DATABASE_URL=mysql://root:root@database_test:3306/mybank_test?serverVersion=9.1.0&charset=utf8mb4
-                CORS_ALLOW_ORIGIN=${CORS_ALLOW_ORIGIN}
-                JWT_SECRET_KEY=${JWT_SECRET_KEY}
-                JWT_PUBLIC_KEY=${JWT_PUBLIC_KEY}
-                JWT_PASSPHRASE=${JWT_PASSPHRASE}
-                ADMIN_EMAIL=boudjemaa.amine.2003@gmail.com
-                ADMIN_PASSWORD=12345678\" > .env.test
-            """
-
-            sh 'cat .env.test'
+//             sh """
+//                 echo \"APP_ENV=test
+//                 KERNEL_CLASS=App\\\\Kernel
+//                 APP_SECRET=${APP_SECRET}
+//                 DATABASE_URL=mysql://root:root@database_test:3306/mybank_test?serverVersion=9.1.0&charset=utf8mb4
+//                 CORS_ALLOW_ORIGIN=${CORS_ALLOW_ORIGIN}
+//                 JWT_SECRET_KEY=${JWT_SECRET_KEY}
+//                 JWT_PUBLIC_KEY=${JWT_PUBLIC_KEY}
+//                 JWT_PASSPHRASE=${JWT_PASSPHRASE}
+//                 ADMIN_EMAIL=boudjemaa.amine.2003@gmail.com
+//                 ADMIN_PASSWORD=12345678\" > .env.test
+//             """
 
             sh '''
                 if [ ! -f config/jwt/private.pem ] || [ ! -f config/jwt/public.pem ]; then
@@ -56,8 +48,8 @@ node("${AGENT_DOCKER}") {
                 done
             '''
 
-           sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && ls"'
-           sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && php bin/console d:m:m"'
+//            sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && ls"'
+//            sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && php bin/console d:m:m"'
 
            sh """
            docker exec api-backend-1 bash -c 'echo \"APP_ENV=test
