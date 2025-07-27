cf2794066f91567052dae598a567d45ac5f4a87e
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-27 14:09:26 +0200

    final

--- jenkins/backend_test.groovy
+++ jenkins/backend_test.groovy
@@ -65,7 +65,7 @@ node("${AGENT_DOCKER}") {
                                                               JWT_PASSPHRASE=${JWT_PASSPHRASE}\" > .env.test'
            """
 
-            sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && sleep 10 && php bin/console php bin/console doctrine:migrations:migrate --env=test"'
+            sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && sleep 10 && php bin/console doctrine:migrations:migrate --env=test"'
             sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && sleep 10 && php bin/console doctrine:schema:update --force --env=test"'
             sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && php bin/console app:create-user --env=test"'
             sh 'docker exec -i api-backend-1 bash -c "cd /var/www/project && php bin/phpunit"'
