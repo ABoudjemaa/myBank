79aef79fc98bc1c535d175b53ebe6450f37c778c
ABoudjemaa <boudjemaa.amine.2003@gmail.com>
2025-07-27 11:56:13 +0200

    -

--- jenkins/frontend_install.groovy
+++ jenkins/frontend_install.groovy
@@ -2,15 +2,11 @@ stage('Clone Frontend Repository') {
     git branch: 'main', url: 'https://github.com/ABoudjemaa/myBank.git'
 }
 
-stage('Install & Run Frontend') {
-    dir('front') {
-        sh 'npm install dotenv'
-        sh 'npm install'
-        sh 'ls -la'
-        sh 'curl http://backend/api'
-        sh "echo 'NEXT_PUBLIC_API_BASE_URL=http://backend/api' > .env"
-        sh 'cat .env'
-        sh 'npx vitest run'
-    }
-    
+dir('front') {
+    sh "echo 'NEXT_PUBLIC_API_BASE_URL=http://backend/api' > .env"
+    sh 'cat .env'
+    sh 'npm install dotenv'  // si vraiment nécessaire, sinon déjà dans package.json
+    sh 'npm install'
+    sh 'npx vitest run'
 }
+
